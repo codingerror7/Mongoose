4c82d5865054dc7a3c0411c32685fabb68c9051a
CodingError7 <144795507+codingerror7@users.noreply.github.com>
2025-06-08 00:16:33 +0530

    index.js

--- index.js
+++ index.js
@@ -85,3 +85,86 @@ let reader3 = new Reader({
  reader1.save();
  reader2.save();
  reader3.save();
+
+
+
+//EXTRA PRACTICE CODE:
+// const mongoose = require("mongoose");
+
+// main().then(()=>{
+//     console.log("SUCCESSFULL!");
+// }).catch((rej)=>{
+//     console.log(rej);
+// })
+
+// async function main(){
+//     await mongoose.connect("mongodb://127.0.0.1:27017/test");
+// }
+
+// const userSchema = new mongoose.Schema({
+//     name : String,
+//     email : String,
+//     age : Number,
+// });
+
+//  const User = mongoose.model("User",userSchema);
+//  User.find({}).then((res)=>{
+//     console.log(res);
+//  }).catch((rej)=>{
+//     console.log(rej);
+//  })
+//  User.deleteMany({name : "sahil khanna"}).then((res)=>{
+//     console.log(res);
+//  }).catch((rej)=>{
+//     console.log(rej);
+//  })
+
+//  User.updateMany({name : "sujal saraswat"},{age : 9300446947}).then((res)=>{
+//     console.log(res);
+//  }).catch((rej)=>{
+//     console.log(rej);
+//  })
+
+//  User.find({}).then((res)=>{
+//     console.log(res);
+//  }).catch((rej)=>{
+//     console.log(rej);
+//  })
+// User.find({}).then((res)=>{
+//     console.log(res);
+// }).catch((rej)=>{
+//     console.log(rej);
+// })
+// User.findOne({name : "abhay singh"}).then((res)=>{
+//     console.log(res);
+// }).catch((rej)=>{
+//     console.log(rej);
+// })
+
+
+// let user1 = new User({
+//     name : "sujal saraswat",
+//     email : "saraswatsujal@gmail.com",
+//     age : 21,
+// });
+// let user2 = new User({
+//     name : "sahil khanna",
+//     email : "khanna6776@gmail.com",
+//     age : 90,
+// })
+// let user3 = new User({
+//     name : "aman chopra",
+//     email : "chopraman@gmail.in.org",
+//     age : 33,
+// })
+// let user4 = new User({
+//     name : "abhay singh",
+//     email : " singhabhay121@gmail.com",
+//     age : 44,
+// })
+// user1.save();
+// user2.save();
+// user3.save();
+// user4.save();
+
+
